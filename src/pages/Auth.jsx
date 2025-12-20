6f05854a17c8af034cb028e73817fbb07f3951cf
k24517165gmailcom <k24517165@gmailcom>
2025-12-20 10:20:11 +0530

    fix

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -23,7 +23,7 @@ const Auth = () => {
     try {
       const response = await fetch(url, {
         method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
       });
 
